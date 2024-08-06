206e2a3a1bcee70e71feefffc14621593de2eba9
tranviettrung <viettrung2580@gmail.com>
2024-08-06 11:23:40 +0700

    merge feature/tags

--- src/components/tag/schema.ts
+++ src/components/tag/schema.ts
@@ -3,7 +3,5 @@ import { z } from "zod";
 export const createTagSchema = z.object({
   name: z.string().min(1).max(100),
   slug: z.string().min(1).max(100),
-  type: z.enum(["SYSTEM", "DEFAULT"]),
-  organizationId: z.number(),
-  userId: z.number(),
+  taxonomyId: z.number(),
 });
